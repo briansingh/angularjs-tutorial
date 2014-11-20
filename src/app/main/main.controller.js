845d0af67115bcf801cb3966ffb1390e17cec172
Brian Singh <briansingh@users.noreply.github.com>
2014-11-19 21:35:04 -0500

    ui work for todo

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,34 +1,45 @@
 'use strict';
 
 angular.module('angularjsTutorial')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
     var self = this;
+
+    self.newTodoTitle = '';
+
     self.todos = [];
+
     self.addTodo = function(todo) {
-
-      var obj = {
-        title : undefined,
-        description : undefined,
-        completed : false,
-        dueDate : undefined
-      };
-
-      if(todo.title === undefined) return;
-
-      obj.title = todo.title;
-      obj.description = todo.description;
-      obj.dueDate = todo.dueDate;
-
-      if(todo.completed !== undefined){
-        obj.completed = todo.completed;
-      }
-      
-      self.todos.push(obj);
+      self.todos.push({
+        title : todo.title,
+        completed : false
+      });
+      self.newTodoTitle = '';
     };
+
     self.removeTodo = function(title) {
       self.todos = self.todos.filter(function(item){
         return item.title !== title;
       });
     };
+
+    self.getToDoClasses = function(todo){
+      return {
+        'completed' : todo.completed
+      }
+    };
+
+    $scope.$watch(function(){
+        return self.newTodoTitle;
+      }, function(newValue, oldValue){
+        console.log('self.newTodoTitle changed', newValue);
+      },
+      true);
+
+    $scope.$watch(function(){
+        return self.todos;
+      }, function(newValue, oldValue){
+        console.log('self.todos changed', newValue);
+      },
+      true);
 
   });
--- test/unit/main.js
+++ test/unit/main.js
@@ -1,41 +1,56 @@
 'use strict';
 
 describe('controllers', function(){
+  var scope;
+
   beforeEach(module('angularjsTutorial'));
 
+  beforeEach(inject(function($rootScope) {
+    scope = $rootScope.$new();
+  }));
+
   it('should have an array of todos', inject(function($controller) {
-    var ctrl = $controller('MainCtrl', {});
-    expect(angular.isArray(ctrl.todos)).toBeTruthy();
+    var mainCtrl = $controller('MainCtrl', {
+      $scope : scope
+    });
+    expect(angular.isArray(mainCtrl.todos)).toBeTruthy();
   }));
 
   it('should be able to add a todo', inject(function($controller) {
-    var ctrl = $controller('MainCtrl', {});
-    ctrl.addTodo({
+    var mainCtrl = $controller('MainCtrl', {
+      $scope : scope
+    });
+    mainCtrl.addTodo({
       title : 'test title'
     });
-    expect(ctrl.todos.length == 1).toBeTruthy();
+    expect(mainCtrl.todos.length == 1).toBeTruthy();
   }));
 
   it('should be able to remove a todo', inject(function($controller) {
-    var ctrl = $controller('MainCtrl', {});
+    var mainCtrl = $controller('MainCtrl', {
+      $scope : scope
+    });
     var title = 'test title';
-    ctrl.addTodo({
+    mainCtrl.addTodo({
       title : 'test title'
     });
-    expect(ctrl.todos.length == 1).toBeTruthy();
-    ctrl.removeTodo(title);
-    expect(ctrl.todos.length == 0).toBeTruthy();
+    expect(mainCtrl.todos.length == 1).toBeTruthy();
+    mainCtrl.removeTodo(title);
+    expect(mainCtrl.todos.length == 0).toBeTruthy();
   }));
 
   it('should create "title" and "completed" properties on todo', inject(function($controller) {
-    var ctrl = $controller('MainCtrl', {});
-    ctrl.addTodo({
+    var mainCtrl = $controller('MainCtrl', {
+      $scope : scope
+    });
+    mainCtrl.addTodo({
       title : 'test title'
     });
-    expect(ctrl.todos.length == 1).toBeTruthy();
-    expect(ctrl.todos[0].title).toBeDefined();
-    expect(ctrl.todos[0].completed).toBeDefined();
-    expect(ctrl.todos[0].completed).toBe(false);
+    expect(mainCtrl.todos.length == 1).toBeTruthy();
+    expect(mainCtrl.todos[0].title).toBeDefined();
+    expect(mainCtrl.todos[0].completed).toBeDefined();
+    expect(mainCtrl.todos[0].completed).toBe(false);
   }));
+
 
 });
